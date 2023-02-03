87391713f0e6e6435ebcf847feb2241af7949154
TomMarsura <simulingytb@gmail.com>
2023-02-03 16:09:24 +0100

    a

--- src/menu.c
+++ src/menu.c
@@ -5,14 +5,20 @@
 
 void initGame(){
     int i, j;
+    /* Initialisation de la matrice à 0 */
     for(i = 0; i < 5; i++){
         for(j = 0; j < 3; j++){
-            mat[i][j] = 0;
+            route[i][j] = 0;
         }
     }
 
     /* Placement de la voiture au milieu de la dernière ligne */
-    mat[4][1] = 1;
+    route[4][1] = 1;
+    score = 0;
+}
+
+void readProfiles(){
+    FILE * fichier = fopen("../save/save.csv", "r");
     
 }
 
