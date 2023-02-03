f6c5187cd044ccdb8b19a4da649d0fd19b6888d3
AyoubV11 <alaaribi36@icloud.com>
2023-02-03 16:31:00 +0100

    obstacle

--- src/game.c
+++ src/game.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 
 
+
+
+void affichae_mat(int mat[5][3]){
+
+    int i,j;
+
+
+    for (i = 0;i< 5;i++){
+
+        for(j = 0;j<3;j++){
+            printf("%d",mat[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+void obstacle(int mat[5][3]){
+
+    /*initialisation*/
+    int obstacle;
+    int nb_obstacle;
+    int cpt = 0;
+
+    /*genere position
+    nb obstacle
+    */
+    
+/*genere le nombre d'obstacle soit 1 ou 2*/
+
+    nb_obstacle = rand() % 2 +1;
+
+    while( cpt != nb_obstacle){
+/*genere un nombre entre 0 et 3 , qui represente l'endroit ou l'obstacle va etre positionner*/
+        obstacle = rand() % 3 +1;
+
+        mat[0][obstacle] = 2;
+        cpt++;
+
+    }
+
+    affichae_mat(mat);
+    
+}
+
 int crash(){
   int i;
   for (i=0;i<LARGEUR-1;i++){
