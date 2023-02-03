9fb959d1bc9f9a261a26146b45f874f5a90842d8
TomMarsura <simulingytb@gmail.com>
2023-02-03 16:18:10 +0100

    readProfiles added

--- lib/menu.h
+++ lib/menu.h
@@ -1,3 +1,4 @@
 int pointsProfils[50] = {}; // Points des 50 profils
 char nomProfils[50][20] = {}; // Noms des 50 profils (20 caractères max)
+int numProfils[50] = {}; // Numéros des 50 profils
 int nbProfils = 0; // Nombre de profils
