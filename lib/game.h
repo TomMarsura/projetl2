1018dfa96b7b2c216f0f81f0a4bbda78ceb0d5bc
tibo <tibo.gasnier@gmail.com>
2023-02-04 00:52:46 +0100

    ajout prototype fonction dans game.h

--- lib/game.h
+++ lib/game.h
@@ -1,5 +1,14 @@
 #define HAUTEUR 5
 #define LARGEUR 3
 
+
+/* Variables globales */
 int route[HAUTEUR][LARGEUR] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
 int score = 0;
+
+
+
+/* Prototype des fonctions */
+void obstacle(int mat[HAUTEUR][LARGEUR]);
+int crash();
+void deplacement();
