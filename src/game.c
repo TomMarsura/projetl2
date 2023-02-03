0fd59b99202357f8bbee61c4c14c53eea7b83c82
tibo <tibo.gasnier@gmail.com>
2023-02-03 23:38:46 +0100

    fonction deplacement

--- src/game.c
+++ src/game.c
@@ -9,6 +9,11 @@
 #include <stdlib.h>
 #include "../lib/game.h"
 
+/*include pour la fonction deplacement*/
+#include <termios.h>
+#include <unistd.h>
+
+
 /**
 *@brief Affiche le contenu d'une matrice
 *@param[in] mat matrice de type int
@@ -58,6 +63,7 @@ void obstacle(int mat[HAUTEUR][LARGEUR]){
  * @return int
 */
 int crash(){
+
   int i;
   for (i=0;i<LARGEUR-1;i++){
     /*verifie si la ligne actuelle contient la voiture et si la la ligne supérieur est un obstacle*/
@@ -69,4 +75,62 @@ int crash(){
   /*on retourne 0 quand il n'y a pas de crash*/
   return 0;
 
+}
+
+
+/**
+ * @brief Fonction deplacement : Cette fonction permet de deplacer la voiture si le joueur appuie sur la flèche de gaudhe ou sur la flèche de droite de son clavier
+ * @return void
+*/
+
+void deplacement(){
+
+  /*La fonction clavier retourne le code ASCII de la touche du clavier sur la laquelle appuie le joueur */
+  int clavier(){
+      struct termios oldattr, newattr;
+      int ch;
+      tcgetattr(STDIN_FILENO, &oldattr);
+      newattr = oldattr;
+      newattr.c_lflag &= ~(ICANON | ECHO);
+      tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+      ch = getchar();
+      tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+      return ch;
+  }
+
+  int c;
+  int i;
+  int x;
+  int y;
+  c = clavier();
+
+  for (i=0;i<LARGEUR-1;i++){
+    /*trouver la postion de la voiture sur la ligne*/
+    if (route[HAUTEUR-1][i] == 1){
+      /*x est la ligne de la position de la voiture*/
+      x = HAUTEUR-1;
+      /*y est la colonne de la position de la voiture*/
+      y = i;
+    }
+  }
+
+  /*si le joueur appuie sur la flèche de droite code ASCII=67*/
+  if (c == 67){
+    /*Verifier si la limite de la route sur la droite est dépassée*/
+    if (!(y == LARGEUR)){
+      /*On modifie la position de la voiture dans la matrice*/
+      route[x][y] = 0;
+      route[x][y+1] = 1;
+    }
+  }
+
+  /*si le joueur appuie sur la flèche de gauche code ASCII=68*/
+  if (c == 68){
+    /*Verifier si la limite de la route sur la gauche est dépassée*/
+    if (!(y == 0)){
+      /*On modifie la position de la voiture dans la matrice*/
+      route[x][y] = 0;
+      route[x][y-1] = 1;
+    }
+  }
 }
