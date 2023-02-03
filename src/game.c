abb1cf37e5fc797b2f19dde277aa5aa334bafb3c
Gasnier Thibaut <s2101104@ic2s120-11.univ-lemans.fr>
2023-02-03 16:04:28 +0100

    modif game.c

--- src/game.c
+++ src/game.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 
+
+int crash(){
+  int i;
+  for (i=0;i<LARGEUR-1;i++){
+    /*verifie si la ligne actuelle contient la voiture et si la la ligne supérieur est un obstacle*/
+    if ((route[HAUTEUR-1][i] == 1) && (route[HAUTEUR-2][i] == 2)){
+      /*on retourne 1 pour signnfier le crash*/
+      return 1;
+    }
+  }
+
