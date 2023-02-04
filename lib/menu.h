da4cfa0d07c2981c67567759939171b2e648e419
AyoubV11 <alaaribi36@icloud.com>
2023-02-04 12:38:21 +0100

    Fonction addScore en cours

--- lib/menu.h
+++ lib/menu.h
@@ -3,3 +3,13 @@ char nomProfils[50][20] = {}; // Noms des 50 profils (20 caractères max)
 int numProfils[50] = {}; // Numéros des 50 profils
 int nbProfils = 0; // Nombre de profils
 int nbPartiesProfils[] = {}; //nombre de partie joué
+
+
+
+
+
+
+/*PROTOTYPE FONCTIONS*/
+void initGame();
+void readProfiles();
+void addScore(int profil):
