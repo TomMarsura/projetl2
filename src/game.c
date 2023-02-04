139fedeebffc256f83cefc2550babe5376f4c338
tibo <tibo.gasnier@gmail.com>
2023-02-04 23:20:51 +0100

    Ajout fonction easy game

--- src/game.c
+++ src/game.c
@@ -9,10 +9,12 @@
 #include <stdlib.h>
 #include "../lib/game.h"
 
-/*Include pour la fonction deplacement*/
+/* Include pour la fonction deplacement */
 #include <termios.h>
 #include <unistd.h>
 
+/* Include pour gérer le temps */
+#include <time.h>
 
 
 
@@ -34,8 +36,8 @@ void affichae_mat(int mat[HAUTEUR][LARGEUR]){
     }
     printf("\n");
     printf("\n");
-
 }
+
 
 /**
  * @brief Fonction obstacle : Cette fonction a pour but de generer aleatoirement des obstacles
@@ -63,6 +65,7 @@ void obstacle(int mat[HAUTEUR][LARGEUR]){
     affichae_mat(mat);
 }
 
+
 /**
  * @brief Fonction crash : Cette fonction permet de savoir si la joueur a touché un obstacle, si oui cela renvoie 1
  * @return int
@@ -71,16 +74,15 @@ int crash(){
 
   int i;
 
-  for (i=0;i<LARGEUR-1;i++){
+  for (i=0; i<LARGEUR-1 ; i++){
     /*Verifie si la ligne actuelle contient la voiture et si la la ligne supérieur est un obstacle*/
-    if ((route[0][i] == 1) && (route[1][i] == 2)){
+    if ((route[HAUTEUR-1][i] == 1) && (route[HAUTEUR-1][i] == 2)){
       /* On retourne 1 pour signnfier le crash */
       return 1;
     }
   }
   /* On retourne 0 quand il n'y a pas de crash */
   return 0;
-
 }
 
 
@@ -108,42 +110,77 @@ void deplacement(){
   int x;
   int y;
 
-  while (1){
 
-    for (i=0;i<LARGEUR;i++){
-      /* Trouver la postion de la voiture sur la ligne */
-      if (route[0][i] == 1){
-        /* La variable x correspond à la ligne de la position de la voiture */
-        x = 0;
-        /* La variable y correspond à la colonne de la position de la voiture */
-        y = i;
-        break;
-      }
+  for (i=0 ; i<LARGEUR ; i++){
+    /* Trouver la postion de la voiture sur la ligne */
+    if (route[HAUTEUR-1][i] == 1){
+      /* La variable x correspond à la ligne de la position de la voiture */
+      x = HAUTEUR-1;
+      /* La variable y correspond à la colonne de la position de la voiture */
+      y = i;
+      break;
     }
+  }
 
-    /* Si le joueur appuie sur la flèche de droite code ASCII=67 et verifier si la limite de la route sur la droite est dépassée */
-    c = clavier();
-    if ((c == 67) && (!(y == LARGEUR-1))){
-      /* On modifie la position de la voiture dans la matrice */
-      route[x][y] = 0;
-      route[x][y+1] = 1;
-      affichae_mat(route);
-    }
+  /* Si le joueur appuie sur la flèche de droite code ASCII=67 et verifier si la limite de la route sur la droite est dépassée */
+  c = clavier();
+  if ((c == 67) && (!(y == LARGEUR-1))){
+    /* On modifie la position de la voiture dans la matrice */
+    route[x][y] = 0;
+    route[x][y+1] = 1;
+    affichae_mat(route);
+  }
 
-    /* Si le joueur appuie sur la flèche de gauche code ASCII=68 et verifier si la limite de la route sur la gauche est dépassée */
-    else if ((c == 68) && (!(y == 0))) {
-      /* On modifie la position de la voiture dans la matrice */
-      route[x][y] = 0;
-      route[x][y-1] = 1;
-      affichae_mat(route);
-    }
+  /* Si le joueur appuie sur la flèche de gauche code ASCII=68 et verifier si la limite de la route sur la gauche est dépassée */
+  else if ((c == 68) && (!(y == 0))){
+    /* On modifie la position de la voiture dans la matrice */
+    route[x][y] = 0;
+    route[x][y-1] = 1;
+    affichae_mat(route);
   }
 }
 
+
+/**
+ * @brief Fonction decalage : Cette fonction permet de décaler tout le contenu de la matrice une ligne plus bas
+ * @return void
+*/
+void decalage(){
+
+  int i;
+  int j;
+
+  for (i=HAUTEUR-1 ; i>0 ; i--){
+    for (j=0 ; j<LARGEUR-1 ; j++){
+      route[i][j] = route[i-1][j];
+    }
+  }
+
+  for (j = 0 ; j < LARGEUR ; j++){
+    route[0][j] = 0;
+  }
+}
+
+
+/**
+ * @brief Fonction easyGame : Cette fonction est la fonction principale qui permet de gerer le jeu en appelant les autres fonctions
+ * @return void
+*/
+void easyGame(){
+
+  /* Tant qu'il n'y a pas de crash */
+  while (crash() == 0){
+    affichae_mat(route);
+    decalage();
+    /* On attend 3 secondes entre chaque décalage */
+    sleep(3);
+  }
+}
+
+
+
 int main(){
 
-  affichae_mat(route);
-  deplacement(route);
-
+  easyGame();
   return 0;
 }
