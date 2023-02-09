2b009d03880d941780c6b9a2996fa64d575353ee
AyoubV11 <alaaribi36@icloud.com>
2023-02-09 11:46:11 +0100

    test game

---
+++ test/test_game.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+#include "../lib/game.h"
+#include "../lib/menu.h"
+
+
+
+int main(){
+    int profile;
+    
+    
+    initGame();
+    affiche_mat(route);
+
+
+
+    return 0;
+}
