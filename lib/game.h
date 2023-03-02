d24c9341c2241d54171d2985b22938b68cfca178
tibo <tibo.gasnier@gmail.com>
2023-03-02 13:51:17 +0100

    game

--- lib/game.h
+++ lib/game.h
@@ -7,13 +7,13 @@
 
 
 /* Variables globales */
-int route[HAUTEUR][LARGEUR] = {{0,5,0}, {6,0,0}, {0,0,7}, {0,8,0}, {0,1,0}};
+int route[HAUTEUR][LARGEUR] = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,1,0}};
 int score = 0;
 int cpt_distance = 0;
 
 
 /* Prototype des fonctions */
-void obstacle(int mat[HAUTEUR][LARGEUR]);
+void obstacle();
 int crash();
 void deplacement();
 int calcul_score();
