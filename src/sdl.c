69c6adb15faa4ac2d65eaf5687ed6567518e3925
TomMarsura <tom.marsura@gmail.com>
2023-03-21 16:30:34 +0100

    createProfil fini

--- src/sdl.c
+++ src/sdl.c
@@ -33,16 +33,19 @@ int creationProfil(SDL_Window* window, SDL_Renderer* renderer){
     //Effacer l'écran
     SDL_RenderClear(renderer);
 
-    // Afficher la texture de fond
-
     SDL_Surface *text_surface = NULL;
     SDL_Texture *text_texture = NULL;
     TTF_Font *font = NULL;
+    TTF_Font *fontLogo = NULL;
+    TTF_Font *fontFooter = NULL;
     SDL_Rect text_rect = {0};
     SDL_Event event;
     int quit = 0;
     char text_input[SIZE_NAME] = {0};
     int text_input_length = 0;
+    int font_size = 30;
+    bool_t nomExist = FAUX;
+    SDL_Texture* textureErreurActuelle = NULL; // Initialiser la texture d'erreur actuelle à NULL
 
     // Initialisation de la SDL_ttf
     if (TTF_Init() != 0) {
@@ -50,12 +53,39 @@ int creationProfil(SDL_Window* window, SDL_Renderer* renderer){
         return 1;
     }
 
+
     // Chargement de la police de caractères
-    font = TTF_OpenFont("../fonts/police.TTF", 28);
+    font = TTF_OpenFont("../fonts/police.TTF", font_size);
+
     if (font == NULL) {
         fprintf(stderr, "Erreur TTF_OpenFont : %s", TTF_GetError());
         return 1;
     }
+
+    fontLogo = TTF_OpenFont("../fonts/police.TTF", font_size + 20);
+
+    if (font == NULL) {
+        fprintf(stderr, "Erreur TTF_OpenFont : %s", TTF_GetError());
+        return 1;
+    }
+
+    fontFooter = TTF_OpenFont("../fonts/police.TTF", font_size - 20);
+
+    if (font == NULL) {
+        fprintf(stderr, "Erreur TTF_OpenFont : %s", TTF_GetError());
+        return 1;
+    }
+
+    // Création de la texture du texte
+    SDL_Color white = {255, 255, 255, 255};
+
+    // Création des textes d'erreur
+    SDL_Surface* erreurNomExist = TTF_RenderText_Blended(font, "Pseudo deja existant", white);
+    SDL_Surface* erreurNomVide = TTF_RenderText_Blended(font, "Veuillez saisir un nom", white);
+
+    // Créer une texture à partir de la surface
+    SDL_Texture* textureErreurNomExist = SDL_CreateTextureFromSurface(renderer, erreurNomExist);
+    SDL_Texture* textureErreurNomVide = SDL_CreateTextureFromSurface(renderer, erreurNomVide);
 
     // Boucle principale
     while (!quit) {
@@ -69,13 +99,8 @@ int creationProfil(SDL_Window* window, SDL_Renderer* renderer){
                     text_input_length++;
                     text_input[text_input_length] = '\0';
 
-                    int font_size = 20;
-                    font = TTF_OpenFont("../fonts/police.TTF", font_size);
-
-                    // Création de la texture du texte
-                    SDL_Color white = {255, 255, 255, 255};
                     //Création d'un rectangle pour le texte de taille 640x480
-                    text_rect.w = 200;
+                    text_rect.w = 300;
                     text_rect.h = 100;
                     text_surface = TTF_RenderUTF8_Blended_Wrapped(font, text_input, white, text_rect.w);
                     text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
@@ -85,13 +110,8 @@ int creationProfil(SDL_Window* window, SDL_Renderer* renderer){
                     text_input_length--;
                     text_input[text_input_length] = '\0';
 
-                    int font_size = 20;
-                    font = TTF_OpenFont("../fonts/police.TTF", font_size);
-
-                    // Création de la texture du texte
-                    SDL_Color white = {255, 255, 255, 255};
                     //Création d'un rectangle pour le texte
-                    text_rect.w = 200;
+                    text_rect.w = 300;
                     text_rect.h = 100;  
                     text_surface = TTF_RenderUTF8_Blended_Wrapped(font, text_input, white, text_rect.w);
                     text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
@@ -99,17 +119,31 @@ int creationProfil(SDL_Window* window, SDL_Renderer* renderer){
                     //Verification si le nom existe déjà
                     for (int i = 0; i < nbProfils; i++){
                         if (strcmp(text_input, nomProfils[i]) == 0){
-                            printf("Ce nom existe déjà, veuillez en choisir un autre");
+                            nomExist = VRAI;
                         }
                     }
-                    //Verification si le nom est vide
-                    if (text_input_length == 0){
-                        printf("Veuillez entrer un nom");
+
+                    printf("\n%d\n", text_input_length);
+                    // Afficher l'erreur de nom vide si nécessaire
+                    if (text_input_length == 0 && nomExist == FAUX) {
+                        textureErreurActuelle = NULL;
+                        textureErreurActuelle = textureErreurNomVide;
                     }
-                    else{
+
+                    // Si le nom est valide, créer le profil et quitter
+                    else if (text_input_length > 0 && nomExist == FAUX) {
                         createProfile(text_input);
                         quit = 1;
                     }
+
+                    // Afficher l'erreur de nom existant si nécessaire
+                    if (nomExist == VRAI) {
+                        textureErreurActuelle = NULL;
+                        textureErreurActuelle = textureErreurNomExist;
+                        nomExist = FAUX;
+                    }
+
+                    
                 }
                 else if (event.key.keysym.sym == SDLK_ESCAPE) {
                     quit = 1;
@@ -118,23 +152,73 @@ int creationProfil(SDL_Window* window, SDL_Renderer* renderer){
         }
         // Effacement de l'écran
         SDL_RenderClear(renderer);
+
+        // Dessine le fond gris
         SDL_SetRenderDrawColor(renderer, 44, 44, 44, 255);
+        SDL_Rect background = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+        SDL_RenderFillRect(renderer, &background);
+
+        // Création du rectangle autour du texte
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_Rect text_back = {(SCREEN_WIDTH - 350) / 2, (SCREEN_HEIGHT - 15) / 2, 350, 50};
+        SDL_RenderDrawRect(renderer, &text_back);
+
+        //Affichage textes
+        SDL_Surface* createProfil = TTF_RenderText_Blended(font, "Ecrivez votre pseudo", white);
+        SDL_Surface* carGame = TTF_RenderText_Blended(fontLogo, "CAR GAME", white);
+        SDL_Surface* escape = TTF_RenderText_Blended(fontFooter, "Touche echap pour revenir au menu principal", white);
+
+        // Créer une texture à partir de la surface
+        SDL_Texture* textureCreateProfil = SDL_CreateTextureFromSurface(renderer, createProfil);
+        SDL_Texture* textureCarGame = SDL_CreateTextureFromSurface(renderer, carGame);
+        SDL_Texture* textureEscape = SDL_CreateTextureFromSurface(renderer, escape);
+        
+        // Créer un rectangle pour la texture
+        SDL_Rect rectCreateProfil = {(SCREEN_WIDTH - createProfil->w) / 2, SCREEN_HEIGHT / 2 - 100, createProfil->w, createProfil->h };
+        SDL_Rect rectCarGame = {(SCREEN_WIDTH - carGame->w) / 2, 100, carGame->w, carGame->h };
+        SDL_Rect rectEscape = {(SCREEN_WIDTH - escape->w) / 2, SCREEN_HEIGHT - 50, escape->w, escape->h};
+
+        // Afficher la texture
+        SDL_RenderCopy(renderer, textureCreateProfil, NULL, &rectCreateProfil);
+        SDL_RenderCopy(renderer, textureCarGame, NULL, &rectCarGame);
+        SDL_RenderCopy(renderer, textureEscape, NULL, &rectEscape);
+
+        // Si une erreur est en cours d'affichage, afficher la texture correspondante
+        if (textureErreurActuelle != NULL) {
+            SDL_Rect rectErreur = {(SCREEN_WIDTH - erreurNomVide->w) / 2, SCREEN_HEIGHT - 200, erreurNomVide->w, erreurNomVide->h };
+            SDL_RenderCopy(renderer, textureErreurActuelle, NULL, &rectErreur);
+        }
 
         // Affichage du texte
         if (text_texture != NULL) {
             SDL_QueryTexture(text_texture, NULL, NULL, &text_rect.w, &text_rect.h);
-            text_rect.x = (640 - text_rect.w) / 2;
-            text_rect.y = (480 - text_rect.h) / 2;
+            text_rect.x = (SCREEN_WIDTH - text_rect.w) / 2;
+            text_rect.y = (SCREEN_HEIGHT) / 2;
             SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
         }
 
         // Rafraîchissement de l'écran
         SDL_RenderPresent(renderer);
+        SDL_DestroyTexture(textureCreateProfil);
+        SDL_DestroyTexture(textureCarGame);
+        SDL_DestroyTexture(textureEscape);
+        SDL_FreeSurface(escape);
+        SDL_FreeSurface(carGame);
+        SDL_FreeSurface(createProfil);
     }
 
     SDL_DestroyTexture(text_texture);
     SDL_FreeSurface(text_surface);
     TTF_CloseFont(font);
+    TTF_CloseFont(fontLogo);
+
+    //Destruction messages d'erreur
+    SDL_DestroyTexture(textureErreurActuelle);
+    SDL_DestroyTexture(textureErreurNomExist);
+    SDL_DestroyTexture(textureErreurNomVide);
+
+    SDL_FreeSurface(erreurNomExist);
+    SDL_FreeSurface(erreurNomVide);
     return 0;
 }
 
