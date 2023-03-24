da050782c0891b2a52ad31d1e5370b23b426f5ec
tibo <tibo.gasnier@gmail.com>
2023-03-24 09:15:25 +0100

    sdl

--- src/game.c
+++ src/game.c
@@ -7,8 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "../lib/game.h"
-#include "../lib/menu.h"
+/*#include "../lib/game.h"*/
+/*#include "../lib/menu.h"*/
 
 /* Include pour la fonction deplacement */
 #include <termios.h>
@@ -18,6 +18,16 @@
 
 /* Include pour gérer le temps */
 #include <time.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_video.h>
+
+#define HAUTEUR 5
+#define LARGEUR 3
+
+#define VITESSE_MAX 0.1
 
 
 
@@ -33,11 +43,9 @@
 *@param[in] mat matrice de type int
 *@return void
 */
-extern void affichae_mat(){
+extern void affiche_mat(){
   /*initialisation*/
     int i,j;
-    system("clear");
-
 
     for (i = 0;i< 5;i++){
 
@@ -46,8 +54,9 @@ extern void affichae_mat(){
         }
         printf("\n");
     }
-
-
+      printf("\n");
+      printf("\n");
+      printf("\n");
 
 }
 
@@ -109,84 +118,45 @@ extern int crash(){
 }
 
 
-/**
- * @brief Fonction clavier : Cette fonction permet de renvoyer le code ASCII de la touche renntré au clavier de manière non bloquante
- * @author Thibaut Gasnier
- * @return int
-*/
-extern int clavier() {
-
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    if (ch != EOF) {
-        ungetc(ch, stdin);
-        return 1;
-    }
-
-    return 0;
-}
-
 
 /**
  * @brief Fonction deplacement : Cette fonction permet de deplacer la voiture si le joueur appuie sur la flèche de gauche ou sur la flèche de droite de son clavier
  * @author Thibaut Gasnier
  * @return void
 */
-extern void deplacement(float vitesse){
+extern void deplacement(int touche){
 
-  time_t start_time = time(NULL);
+  int c;
+  int i;
+  int x;
+  int y;
 
-  while ((time(NULL) - start_time) < vitesse) {
-
-    int c;
-    int i;
-    int x;
-    int y;
-
-
-    for (i=0 ; i<LARGEUR ; i++){
-      /* Trouver la postion de la voiture sur la ligne */
-      if (route[HAUTEUR-1][i] == 1){
-        /* La variable x correspond à la ligne de la position de la voiture */
-        x = HAUTEUR-1;
-        /* La variable y correspond à la colonne de la position de la voiture */
-        y = i;
-        break;
-      }
+  for (i=0 ; i<LARGEUR ; i++){
+    /* Trouver la postion de la voiture sur la ligne */
+    if (route[HAUTEUR-1][i] == 1){
+       /* La variable x correspond à la ligne de la position de la voiture */
+       x = HAUTEUR-1;
+      /* La variable y correspond à la colonne de la position de la voiture */
+      y = i;
+      break;
     }
+  }
 
-    if (clavier()){
-      /* Si le joueur appuie sur la flèche de droite code ASCII=67 et verifier si la limite de la route sur la droite est dépassée */
-      c = getchar();
-      if ((c == 67) && (!(y == LARGEUR-1))){
-        /* On modifie la position de la voiture dans la matrice */
-        route[x][y] = 0;
-        route[x][y+1] = 1;
-        affichae_mat();
-      }
+  /* Si le joueur appuie sur la flèche de gauche et verifier si la limite de la route sur la gauche est dépassée */
+  if ((touche == 1) && (!(y == LARGEUR-1))){
+    /* On modifie la position de la voiture dans la matrice */
+    route[x][y] = 0;
+    route[x][y+1] = 1;
+    affiche_mat();
+  }
 
-      /* Si le joueur appuie sur la flèche de gauche code ASCII=68 et verifier si la limite de la route sur la gauche est dépassée */
-      else if ((c == 68) && (!(y == 0))){
-        /* On modifie la position de la voiture dans la matrice */
-        route[x][y] = 0;
-        route[x][y-1] = 1;
-        affichae_mat();
-      }
-    }
+  /* Si le joueur appuie sur la flèche de gauche et verifier si la limite de la route sur la gauche est dépassée */
+  if ((touche == 2) && (!(y == 0))){
+  /* On modifie la position de la voiture dans la matrice */
+    route[x][y] = 0;
+    route[x][y-1] = 1;
+    affiche_mat();
+
   }
 }
 
@@ -232,7 +202,7 @@ extern void decalage(){
 
   /* On met la voiture a lligne du dessus */
   route[x][y] = 1;
-  affichae_mat();
+  affiche_mat();
 }
 
 
@@ -242,7 +212,7 @@ extern void decalage(){
 */
 extern void easyGame(int profil){
 
-  affichae_mat();
+  affiche_mat();
 
   float vitesse = 1.50;
 
@@ -253,7 +223,7 @@ extern void easyGame(int profil){
     if (crash() == 1){
       printf("CRASH !!\n");
       printf("SCORE : %d \n",score);
-      pointsProfils[profil] += score;
+      /*pointsProfils[profil] += score;*/
       break;
     }
     else{
@@ -268,6 +238,98 @@ extern void easyGame(int profil){
     }
   }
 }
+
+
+int main(int argc, char* argv[]) {
+    // Initialiser SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    IMG_Init(IMG_INIT_JPG);
+
+    // Créer une fenêtre SDL
+    SDL_Window* window = SDL_CreateWindow("GAME CAR", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, 0);
+
+    SDL_Surface* car = IMG_Load("../img/car.png");
+    SDL_Renderer * renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_SOFTWARE);
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,car);
+
+    SDL_Surface* fond = IMG_Load("../img/route.png");
+    SDL_Texture* textureRoute = SDL_CreateTextureFromSurface(renderer,fond);
+
+    SDL_Rect rectangle;
+
+    rectangle.w = rectangle.h = 100; // Taille de chaque case de la matrice
+
+    // Calculer la position de départ du rectangle pour centrer la matrice
+    int startX = (800 - LARGEUR * 135) / 2;
+    int startY = (800 - HAUTEUR * 100) / 2;
+
+    SDL_RenderCopy(renderer, textureRoute, NULL, NULL); // Dessiner l'image de fond
+
+
+    SDL_Event event;
+    int quit = 1;
+
+    float vitesse = 1.50;
+
+    affiche_mat();
+
+
+    while (quit) {
+      while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+          quit = 0;
+        }
+        if (event.type == SDL_KEYDOWN) {
+          switch (event.key.keysym.sym) {
+            case SDLK_RIGHT:
+              deplacement(1);
+              break;
+
+            case SDLK_LEFT:
+              deplacement(2);
+              break;
+
+            default:
+              break;
+          }
+        }
+      }
+
+    SDL_RenderCopy(renderer, textureRoute, NULL, NULL);
+
+
+    /* Dessiner l'image de la voiture pour chaque case de la matrice qui contient un 1 */
+    for (int i = 0; i < HAUTEUR; i++) {
+        for (int j = 0; j < LARGEUR; j++) {
+            if (route[i][j] == 1) {
+                rectangle.x = startX + j * 160; // Position horizontale de la case
+                rectangle.y = startY + i * 100; // Position verticale de la case
+                SDL_RenderCopy(renderer, texture, NULL, &rectangle);
+            }
+        }
+    }
+    /* Afficher le rendu à l'écran */
+    SDL_RenderPresent(renderer);
+  }
+
+
+    // Libérer la mémoire allouée pour l'image et la fenêtre SDL
+    SDL_FreeSurface(car);
+    SDL_FreeSurface(fond);
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyTexture(textureRoute);
+
+    SDL_DestroyWindow(window);
+
+    // Quitter SDL et SDL_image
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
+}
+
 
 
 
